d7d3a9a9176221517fda943751871a458e31c934
Alex Hanna <alex.hanna@gmail.com>
2013-08-16 04:16:16 +0400

    A few edits to mention mapper

--- bin/mentionMapper.py
+++ bin/mentionMapper.py
@@ -11,19 +11,28 @@ def main():
         try:
             data = json.loads(line)
         except ValueError as detail:
-            sys.stderr.write(detail.__str__() + "\n")
             continue
 
-        if 'entities' in data and len(data['entities']['user_mentions']) > 0:
-            user          = data['user']
-            user_mentions = data['entities']['user_mentions']
+        if not (isinstance(data, dict)):
+            ## not a dictionary, skip
+            pass
+        elif 'delete' in data:
+            ## a delete element, skip for now.
+            pass
+        elif 'user' not in data:
+            ## bizarre userless edge case
+            pass
+        else:
+            if 'entities' in data and len(data['entities']['user_mentions']) > 0:
+                user          = data['user']
+                user_mentions = data['entities']['user_mentions']
 
-            for u2 in user_mentions:                
-                print "\t".join([
-                    user['id_str'],
-                    u2['id_str'],
-                    "1"
-                    ])
+                for u2 in user_mentions:                
+                    print "\t".join([
+                        user['id_str'],
+                        u2['id_str'],
+                        "1"
+                        ])
 
 if __name__ == '__main__':
     main()
